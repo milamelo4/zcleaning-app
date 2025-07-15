45d29e30556185498399f70cbb07e9672c6904d4
Camila Melo <milamelofl@yahoo.com>
2025-07-15 14:17:25 -0600

    debug

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      secure: process.env.NODE_ENV === "production",
+      secure: false, // set to true again after testing
       sameSite: "lax",
     },
   })
